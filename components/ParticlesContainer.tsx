baf5b4f8813d0035958a002d28e7bf4f7e4d61fe
Dashnyam Batbayar <gfieldmn@gmail.com>
2024-04-02 12:29:00 +1100

    Update ParticlesContainer.tsx

--- components/ParticlesContainer.tsx
+++ components/ParticlesContainer.tsx
@@ -7,7 +7,7 @@ import MediaQuery from "react-responsive";
 const ParticlesContainer = () => {
     const isMobile = MediaQuery({ query: "(max-width: 768px)" });
 
-    const quantity = isMobile ? 100 : 300;
+    const quantity = isMobile ? 10 : 300;
     return (
     <Particles quantity={quantity} className="md:absolute fixed inset-0 -z-10 animate-fade-in"/>
   )
