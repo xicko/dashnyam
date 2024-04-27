45a52c669ccc1dcc4771fcd7512d8d1f71d99fc1
Dashnyam Batbayar <gfieldmn@gmail.com>
2024-04-28 00:03:16 +1000

    assets modal buttons styling adjust

--- pages/assets.tsx
+++ pages/assets.tsx
@@ -157,7 +157,7 @@ const GraphicAssetsPage: React.FC = () => {
             <AnimatedComponent key={asset.id}>
             <div
               key={asset.id}
-              className="ease duration-150 transform scale-100 hover:scale-[1.06] bg-white bg-opacity-[0.9]  pb-4 rounded-3xl shadow-md hover:cursor-pointer">
+              className="ease duration-150 md:transform scale-100 md:hover:scale-[1.06] bg-white bg-opacity-[0.9]  pb-4 rounded-3xl shadow-md hover:cursor-pointer">
               <div onClick={() => openModal(asset)}>
                 <div
                   onClick={() => openModal(asset)}
@@ -217,14 +217,14 @@ const GraphicAssetsPage: React.FC = () => {
 
                 <div className="flex justify-between mt-0">
                   <a download className='' href={selectedAsset.downloadUrl}>
-                    <button className="flex items-center md:text-2xl -ml-8 -mb-8 space-x-[6px] w-fit md:border-[4px] border-[0px] border-white bg-blue-500 hover:bg-blue-700 transition duration-500 ease text-white font-semibold md:py-4 py-3 md:px-8 px-6 rounded-[0px_12px_0px_24px]">
+                    <button className="flex items-center md:text-2xl -ml-8 -mb-8 space-x-[6px] w-fit md:border-[4px] border-[3px] border-white bg-blue-500 hover:bg-blue-700 transition duration-500 ease text-white font-semibold md:py-4 py-3 md:px-8 px-6 rounded-[3px_12px_3px_24px]">
                       <span>Download</span>
                       <svg xmlns="http://www.w3.org/2000/svg" width={20} height={22} fill="white" viewBox="0 0 512 512"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
                     </button>
                   </a>
 
                   <button
-                    className="bg-slate-700 hover:bg-slate-900 md:text-2xl -mr-8 -mb-8 md:border-[4px] border-[0px] border-white transition duration-500 ease text-white font-semibold md:py-4 py-3 md:px-8 px-6 rounded-[12px_0px_24px_0px]"
+                    className="bg-slate-700 hover:bg-slate-900 md:text-2xl -mr-8 -mb-8 md:border-[4px] border-[3px] border-white transition duration-500 ease text-white font-semibold md:py-4 py-3 md:px-8 px-6 rounded-[12px_3px_24px_3px]"
                     onClick={closeModal}
                   >
                     Close
