99e72228edb6e6c6c7fe237786252c2d88e81d88
Dashnyam Batbayar <gfieldmn@gmail.com>
2024-06-10 15:55:05 +1000

    Particles dynamic import removed

--- components/ParticlesContainer.tsx
+++ components/ParticlesContainer.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic';
 import React from 'react'
 import { useMediaQuery } from 'react-responsive';
-const Particles = dynamic(() => import("./Particles"), { ssr: false });
+import Particles from '@components/Particles';
 
 const ParticlesContainer = () => {
     const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
