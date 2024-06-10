545c149542bc16549d9378659683e72b22c63700
Dashnyam Batbayar <gfieldmn@gmail.com>
2024-06-10 15:48:10 +1000

    Dynamically import components 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { AppProps } from "next/app";
 import { initGA, logPageView } from "../utils/analytics";
 import dynamic from "next/dynamic";
-import NavBar from "@components/NavBar";
-import ParticlesContainer from "@components/ParticlesContainer";
 import "app/globals.css";
 import "@app/locomotive-scroll.css";
 
+const ParticlesContainer = dynamic(() => import("@components/ParticlesContainer"), { ssr: false });
+const NavBar = dynamic(() => import("@components/NavBar"), { ssr: false });
 const Locomotive = dynamic(() => import("@components/LocomotiveScroll"), {
   ssr: false,
 });
