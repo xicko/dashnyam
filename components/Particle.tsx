7fd223d3797719645c2075913c722d6a985dab58
Dashnyam Batbayar <gfieldmn@gmail.com>
2024-06-16 19:49:04 +1000

    Implemented light mode by default, separated hero image as its own component, fixed a type error in particle.tsx

--- components/Particle.tsx
+++ components/Particle.tsx
@@ -118,7 +118,7 @@ export default function Particle({
 		};
 	};
 
-	const drawCircle = (circle, update = false) => {
+	const drawCircle = (circle: Circle, update = false) => {
 		if (context.current) {
 		  const { x, y, translateX, translateY, size, alpha } = circle;
 		  
